1b32c2710ffc4aee9a0b3874281fcd7500c95169
Tiff Zhang <zhangtiff@gmail.com>
2016-02-24 16:03:51 -0500

    Changed pet shop buy button to work without changing page.

--- aywas/pet-shop-buy-button.user.js
+++ aywas/pet-shop-buy-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pet Shop Buy Button
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Tiff Zhang
 // @match        http://www.aywas.com/search/*
@@ -13,7 +13,7 @@ var i = 0;
 
 $(document).bind('DOMNodeInserted', function(e) {
     var target = $(e.target);
-    
+
     if (target.hasClass("pet-area-search")) {
         if (target.children(".tiffs-buy-button").length < 1) {
             token = "";
@@ -23,31 +23,34 @@ $(document).bind('DOMNodeInserted', function(e) {
             var petid = petregex.exec(target.html())[1];
             var url = "http://www.aywas.com/pet_shops/buy/?shopid=%SHOPID%&petid=" + petid;
             shop_url = shop_url[0];
-            
+
             target.append('<form action="' + url + '" id="tiff-form-' + i + '" method="post"><input class="tiffs-buy-button" type="hidden" name="token" value="%TOKEN%" />' +
                           '<a href="#" id="tiff-button-' + i + '">Buy Pet</a></form>');
             var g = i;
-            var callback = function (url) {
-                $("#tiff-form-" + g).attr("action", url);
-                $("#tiff-form-" + g).submit();
-            }
-            
+
+
+            var submit_form = function (petid, shopid, token) {
+                var url = "http://www.aywas.com/pet_shops/buy/?shopid=" + shopid + "&petid=" + petid;
+                $.post(url, {"token" : token}, function (data) {
+                    var success = $(data).find("#content .page-notice.success");
+                    if (success.length) {
+                        success.append("<br />You have bought <a target='_blank' href='http://www.aywas.com/pp/view/" + petid + "/'>pet " + petid + "</a>");
+                        $("#content").prepend(success.prop('outerHTML'));
+                    } else {
+                        $("#content").prepend("<div class='page-notice error'>You have failed to buy <a target='_blank' href='http://www.aywas.com/pp/view/"
+                                              + petid + "/'>pet " + petid + "</a></div>");
+                    }
+
+                });
+            };
+
             $("#tiff-button-" + i).click(function() {
-               get_token(shop_url, petid, callback);
+               get_token(shop_url, petid, submit_form);
             });
-            
-            i++; 
+
+            i++;
         } 
     }
-    /*
-    if ($(".tiffs-buy-button").length == $(".pet-area-search").length && $(".pet-area-search").length > 0 && token == "") {
-        $.get(shop_url, function (data) {
-            var tokenregex = /name="token" value="(\w+)"/g;
-            token = tokenregex.exec(data)[1];
-            $(".tiffs-buy-button").val(token);
-        });
-    }
-    */
 });
 
 function get_token(shop_url, petid, callback) {
@@ -55,9 +58,7 @@ function get_token(shop_url, petid, callback) {
         var tokenregex = /name="token" value="(\w+)"/g;
         var shopregex = /\/pet_shops\/buy\/\?shopid=(\d+)&petid=(\d+)/g;
         var shopid = shopregex.exec(data)[1];
-        token = tokenregex.exec(data)[1];
-        var url = "http://www.aywas.com/pet_shops/buy/?shopid=" + shopid + "&petid=" + petid;
-        $(".tiffs-buy-button").val(token);
-        callback(url);
+        var token = tokenregex.exec(data)[1];
+        callback(petid, shopid, token);
     });
 };
